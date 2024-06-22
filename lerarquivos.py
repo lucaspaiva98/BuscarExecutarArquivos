d98c35034862f56d9001808b2ef5a7c43c6b4d19
Lucas Paiva <lucas.rds998@gmail.com>
2024-06-22 15:37:20 -0300

    1.1 Funcionou sem ['']

--- lerarquivos.py
+++ lerarquivos.py
@@ -1,42 +1,32 @@
-from buscadorArquivo import buscadorDeArquivo
 import pandas as pd
 import os
 
-class DadosFuncionario:
-    def __init__(self):
-        self.nome = []
-        self.setor = []
-        self.funcao = []
-        self.celular = []
-        self.telefone = []
-        self.ramal = []
-        self.email = []
+def executarleitura():
+    # Abrir o arquivo
+    file_path = os.path.join(os.getcwd(), 'Dados p. preencher.xlsx')
+    data = pd.read_excel(file_path, dtype=str)  # tudo como texto
 
-    def executarleitura(self):
-        # Abrir o arquivo
-        file_path = buscadorDeArquivo()
-        data = pd.read_excel(file_path, dtype=str)  # tudo como texto
+    # Dados do usuário
+    nome = data['Nome_Funcionario'].tolist()
+    setor = data['Setor'].tolist()
+    funcao = data['Funcao'].tolist()
+    celular = data['Celular Institucional'].tolist()
+    telefone = data['Contato_Empresarial'].tolist()
+    ramal = data['Ramal'].tolist()
+    email = data['Email'].tolist()
 
-        # Dados do usuário
-        self.nome = data['Nome_Funcionario'].tolist()
-        self.setor = data['Setor'].tolist()
-        self.funcao = data['Funcao'].tolist()
-        self.celular = data['Celular Institucional'].tolist()
-        self.telefone = data['Contato_Empresarial'].tolist()
-        self.ramal = data['Ramal'].tolist()
-        self.email = data['Email'].tolist()
+    return nome, setor, funcao, celular, telefone, ramal, email
 
-    def exibir_dados(self):
-        # Imprimir dados do usuário
-        print("Nome:", ', '.join(self.nome))
-        print("Setor:", ', '.join(self.setor))
-        print("Função:", ', '.join(self.funcao))
-        print("Celular:", ', '.join(self.celular))
-        print("Telefone:", ', '.join(self.telefone))
-        print("Ramal:", ', '.join(self.ramal))
-        print("E-mail:", ', '.join(self.email))
+def exibir_dados(nome, setor, funcao, celular, telefone, ramal, email):
+    # Imprimir dados do usuário
+    print("Nome:", ', '.join(nome))
+    print("Setor:", ', '.join(setor))
+    print("Função:", ', '.join(funcao))
+    print("Celular:", ', '.join(celular))
+    print("Telefone:", ', '.join(telefone))
+    print("Ramal:", ', '.join(ramal))
+    print("E-mail:", ', '.join(email))
 
 if __name__ == "__main__":
-    df = DadosFuncionario()
-    df.executarleitura()
-    df.exibir_dados()
+    nome, setor, funcao, celular, telefone, ramal, email = executarleitura()
+    exibir_dados(nome, setor, funcao, celular, telefone, ramal, email)
