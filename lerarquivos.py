2a42d87179c1793d33d025531d8419ca4a099cd7
Lucas Paiva <lucas.rds998@gmail.com>
2024-06-22 15:46:29 -0300

    1.3

--- lerarquivos.py
+++ lerarquivos.py
@@ -1,12 +1,14 @@
-import pandas as pd
-import os
+from buscadorArquivo import buscadorDeArquivo  # Importa a função de buscadorDeArquivo do arquivo buscadorArquivo.py
+import pandas as pd  # Importa a biblioteca pandas
+import os  # Importa a biblioteca os
 
 def executarleitura():
     # Abrir o arquivo
+    # file_path = buscadorDeArquivo()
     file_path = os.path.join(os.getcwd(), 'Dados p. preencher.xlsx')
-    data = pd.read_excel(file_path, dtype=str)  # tudo como texto
+    data = pd.read_excel(file_path, dtype=str)  # leitura do arquivo excel em formato texto
 
-    # Dados do usuário
+    # Dados do usuário para lsita
     nome = data['Nome_Funcionario'].tolist()
     setor = data['Setor'].tolist()
     funcao = data['Funcao'].tolist()
@@ -19,13 +21,13 @@ def executarleitura():
 
 def exibir_dados(nome, setor, funcao, celular, telefone, ramal, email):
     # Imprimir dados do usuário
-    print("Nome:", ', '.join(nome))
-    print("Setor:", ', '.join(setor))
-    print("Função:", ', '.join(funcao))
-    print("Celular:", ', '.join(celular))
-    print("Telefone:", ', '.join(telefone))
-    print("Ramal:", ', '.join(ramal))
-    print("E-mail:", ', '.join(email))
+    print("{}, {}".format(nome[0], nome[1]))
+    print("{}, {}".format(setor[0], setor[1]))
+    print("{}, {}".format(funcao[0], funcao[1]))
+    print("{}, {}".format(celular[0], celular[1]))
+    print("{}, {}".format(telefone[0], telefone[1]))
+    print("{}, {}".format(ramal[0], ramal[1]))
+    print("{}, {}".format(email[0], email[1]))
 
 if __name__ == "__main__":
     nome, setor, funcao, celular, telefone, ramal, email = executarleitura()
